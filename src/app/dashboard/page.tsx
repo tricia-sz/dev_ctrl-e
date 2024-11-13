3d39cfcd9fd01c235eff035341b01a54515966cf
Patricia Souza <patricia.sz.br@gmail.com>
2024-11-13 08:55:49 -0300

    Criando rotas protegidas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,14 @@
 import { Container } from "@/components/container";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    const session = await getServerSession(authOptions)
+    if(!session || session.user) {
+        redirect("/")
+    }
+    
     return (
         <Container>
             <h1>Pagina Dashboard</h1>
